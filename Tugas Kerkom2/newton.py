ac816ffde932b0c3a761a04040f7ba1e7d87c1b3
iynsys <irgiyansy@gmail.com>
2022-10-25 17:16:08 +0700

    tugas 2 kerkom

--- Tugas Kerkom2/newton.py
+++ Tugas Kerkom2/newton.py
@@ -4,13 +4,13 @@ f_turun = lambda x : (6*(x**2)) - (6*x) + 1
 def newtonRaph(x,e = 0.00001):
     i = 0
     x0 = x
-    print("%-10s %-8s\t %-10s %-10s" % ("xk", "fx", "f`x", "Em"))
+    print("%-10s | %-8s\t | %-10s | %-10s" % ("xk", "f(x)", "f`(x)", "Em"))
     xk1 = x0
     em = 0
     while True :
         fx = f_rill(xk1)
         faks = f_turun(xk1)
-        print("%-0.4f %2s  %-0.4f %2s %-0.4f %2s %-0.4f" %(xk1,"", fx,"\t", faks, "" ,em))
+        print("%-0.4f %3s | %-0.4f %2s | %-0.4f %2s | %-0.4f" %(xk1,"", fx,"\t", faks, "" ,em))
         x0 = xk1
         xk1 = xk1  - (fx / faks)
 
