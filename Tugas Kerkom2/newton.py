33708e0c31a267b6483cf17c75d3c24f435374fd
iynsys <irgiyansy@gmail.com>
2022-10-26 08:44:42 +0700

    Tugas kerkom 2

--- Tugas Kerkom2/newton.py
+++ Tugas Kerkom2/newton.py
@@ -1,22 +1,27 @@
-f_rill = lambda x : (2*(x**3)) - (3*(x**2)) + (1*x) - 5
-f_turun = lambda x : (6*(x**2)) - (6*x) + 1
+import timeit
+# Newton Rapson
+f_rill = lambda x : 4.15*x**5 - 2.53*x**3 - 6.35
+f_turun = lambda x : 20.75*x**4 - 7.59*x**2
 
-def newtonRaph(x,e = 0.00001):
+def newtonRaph(x,e = 0.000001):
+    start = timeit.default_timer()
     i = 0
     x0 = x
-    print("%-10s | %-8s\t | %-10s | %-10s" % ("xk", "f(x)", "f`(x)", "Em"))
+    print("\n%-1s | %-10s | %-15s | %-12s  | %-10s" % ("i", "xk", "f(x)", "f`(x)", "Em"))
     xk1 = x0
     em = 0
     while True :
         fx = f_rill(xk1)
         faks = f_turun(xk1)
-        print("%-0.4f %3s | %-0.4f %2s | %-0.4f %2s | %-0.4f" %(xk1,"", fx,"\t", faks, "" ,em))
+        print("%-1s | %-0.4f %3s | %-0.4f %1s | %-0.4f \t | %-0.4f" %(i,xk1,"", fx,"\t", faks ,em))
         x0 = xk1
         xk1 = xk1  - (fx / faks)
 
         em = abs(xk1 - x0)
         i += 1
-        if(i > 6 or em < e) : break;
+        if(i > 10 or em < e) : break;
+    stop = timeit.default_timer()
+    print('\nWaktu Eksekusi Newton: ', stop - start, "\n")  
 
 
 x = float(input("Inputkan nilai x0 = "))
---
+++ Tugas Kerkom2/secant.py
@@ -0,0 +1,28 @@
+import timeit
+f = lambda x : (4.15*(x**5)) - (2.53*(x**3)) - 6.35
+
+def secant(x0, x1 , e=0.00001):
+    xbef = x0
+    xaft = x1
+    fbef = f(xbef)
+    faft = f(xaft)
+    em = 0
+    i = 0
+    print("\n%2s | %2s \t | %2s \t | %2s" % ("i", "x", "f(x)", "Em"))
+    while True :
+        start = timeit.default_timer()
+        print("%2s | %-0.4f \t | %-0.4f \t | %-0.4f" % (i, xbef, fbef, em))
+        em = abs(xaft - xbef)
+        xtemp = xaft
+
+        xaft = xaft-(faft)*(xbef-xaft)/(fbef-faft)
+        xbef = xtemp
+        fbef = f(xbef)
+        faft = f(xaft)
+        i += 1
+        if(i > 10 or em < e ): break;
+    stop = timeit.default_timer()
+    print('\nWaktu Eksekusi Secant: ', stop - start, "\n")
+satu = float(input("Input kan nilai kesatu\t= "))
+dua = float(input("Input kan nilai kedua\t= "))
+secant(satu,dua)
